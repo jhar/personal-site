4e8a0a638e2d90f372c3d639917e6aa32ce275a8
jhar <jhar-dev@protonmail.com>
2025-03-15 15:57:36 -0700

    dark mode footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,16 @@
 export default function Footer() {
   return (
-    <footer class="bg-white fixed bottom-0 w-full">
-      <div class="font-serif flex flex-row items-center justify-around m-auto max-w-5xl py-3 text-medgray text-sm w-[86%]">
+    <footer class="fixed bottom-0 w-full">
+      <div class="font-serif flex flex-row items-center justify-around m-auto max-w-5xl py-3 text-medgray dark:text-offwhite text-sm w-[86%]">
         <span>&copy; {new Date().getFullYear()}</span>
         <a
-          class="hover:animate-pulse hover:text-blue"
+          class="dark:hover:text-lightblue hover:animate-pulse hover:text-blue dark:text-offwhite"
           href="https://github.com/jhar"
         >
           github
         </a>
         <a
-          class="hover:animate-pulse hover:text-blue"
+          class="dark:hover:text-lightblue hover:animate-pulse hover:text-blue dark:text-offwhite"
           href="https://www.linkedin.com/in/justinadenharrison"
         >
           linkedin
