47a37cf4db11b8cb4f19796dd8236068da60eb99
jhar <jhar-dev@protonmail.com>
2025-03-16 11:50:19 -0700

    fix footer bg

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import FooterLink from "./FooterLink.tsx";
 
 export default function Footer() {
   return (
-    <footer class="fixed bottom-0 w-full">
+    <footer class="dark:bg-slate bg-white dark:bg fixed bottom-0 w-full">
       <div class="font-serif flex flex-row items-center justify-around m-auto max-w-5xl py-3 text-medgray dark:text-offwhite text-sm w-[86%]">
         <span>&copy; {new Date().getFullYear()}</span>
         <FooterLink name="credly" href="https://www.credly.com/users/justin-harrison.19866e97" />
