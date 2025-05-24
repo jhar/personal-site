e1ee57fd8aa6abb0b8d5cdb4037be6d8e882e5b4
jhar <jhar-dev@protonmail.com>
2025-05-24 00:46:54 -0700

    remove google tag

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,5 +1,4 @@
 import { PageProps } from "$fresh/server.ts";
-import GoogleTag from "../islands/GoogleTag.tsx";
 
 export default function App({ Component }: PageProps) {
   return (
@@ -9,7 +8,6 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Justin Harrison</title>
       </head>
-      <GoogleTag />
       <Component />
     </html>
   );
