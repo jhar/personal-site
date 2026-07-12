0963f9ae46c347cfdc6562a913ec16ac61506860
Justin Harrison <jhar-dev@protonmail.com>
2026-07-12 13:26:35 -0700

    tenor2

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <div class="absolute top-32 left-0 right-0 bottom-11">
       <img
-        alt="Elmo in flames, raising his arms in triumph."
+        alt="Elmo feeling the power of Jesus flow through him."
         class="absolute h-full object-cover w-full"
-        src="/tenor.gif"
+        src="/tenor2.gif"
       />
     </div>
   );
